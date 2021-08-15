97749f3123a67ed5bd10bf7080d63d4afa3033c8
royce79-creator <connorsboyce07@gmail.com>
2021-08-14 18:02:16 -0700

    Made changes to the front end

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 // TODO: wrap everything in Auth0
+//Done with it
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-royce79-creator.us.auth0.com"
+    clientId="LSBPnWLveilVZJ6CR0Tq5beyqnlnKQnZ"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
